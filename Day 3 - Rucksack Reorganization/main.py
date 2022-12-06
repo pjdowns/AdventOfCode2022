97d632d7b607df1be3c5662e1acdce0434bb0980
pjdowns <PJ_Downs@hotmail.com>
2022-12-05 23:51:56 -0500

    Update main.py
    Still working on figuring out total priority.

--- Day 3 - Rucksack Reorganization/main.py
+++ Day 3 - Rucksack Reorganization/main.py
@@ -3,13 +3,35 @@
 #
 # PJD
 
+priority = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+            "u", "v", "w", "x", "y", "z"]
 
 if __name__ == '__main__':
     data_input = []
     with open("day3_input.txt") as f:
         data_input = f.readlines()
 
+    priority_total = 0
+    common_item = ""
+
     for data in data_input:
         rucksack = data
-        compartments = [rucksack[0:(len(rucksack) / 2)], rucksack[(len(rucksack) / 2):len(rucksack)]]
-        
+        rucksack_end = len(rucksack) - 1
+        rucksack_mid = int(rucksack_end / 2)
+
+        compartments = [rucksack[0:rucksack_mid], rucksack[rucksack_mid:rucksack_end]]
+
+        for item in compartments[0]:
+            if compartments[1].find(item) > 0:
+                common_item = item
+                break;
+
+        if common_item.isupper():
+            priority_total = priority_total + (priority.index(common_item.lower()) + 1)+26
+        else:
+            priority_total = priority_total + (priority.index(common_item)+1)
+
+    print("Priority Total: {0}".format(priority_total))
+
+
